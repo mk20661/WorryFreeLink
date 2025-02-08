120d51a9c4a94cdb80668144c8ffc337299f68dc
Hongbing  Qiu <qakightbing@163.com>
2025-02-08 19:13:54 +0000

    add 16led in to code, and modfiy the all  perviously test code

--- MCU_code/Test_code/FreeRTOS_LED_Test/FreeRTOS_LED_Test.ino
+++ MCU_code/Test_code/FreeRTOS_LED_Test/FreeRTOS_LED_Test.ino
@@ -1,67 +1,138 @@
-#include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
-#define LED_Older_1 1   // LED 1
-#define LED_Older_2 25  // LED 2
-#define LED_Kid_1 26    // LED 3
+#define LED_Older_1 6
+#define LED_Older_2 7
+#define LED_Older_3 8
+#define LED_Older_4 9
+#define LED_Older_5 10
 
-// 任务句柄
-TaskHandle_t blinkTwoLEDsHandle;
-TaskHandle_t blinkSingleLEDHandle;
+#define LED_Kid_1 11
+#define LED_Kid_2 12
+#define LED_Kid_3 13
 
-// 任务1: 两个 LED (LED_Older_1, LED_Older_2) 同时闪烁 5 秒
-void blinkTwoLEDs(void *parameter) {
+#define LED_Older_6 0
+#define LED_Older_7 1
+#define LED_Older_8 2
+#define LED_Older_9 3
+#define LED_Older_10 4
+
+#define LED_Kid_4 A3
+#define LED_Kid_5 A4
+#define LED_Kid_6 A5
+
+#define Button 5
+
+
+TaskHandle_t blinkOlder_1LEDHandle;
+TaskHandle_t blinkOlder_2LEDHandle;
+TaskHandle_t blinkKid_1LEDHandle;
+TaskHandle_t blinkKid_2LEDHandle;
+
+
+void blinkOlder_1LED(void *parameter) {
     while (1) {
-        for (int i = 0; i < 10; i++) { // 5s 闪烁（500ms * 10 次）
+        for (int i = 0; i < 10; i++) { 
             digitalWrite(LED_Older_1, HIGH);
             digitalWrite(LED_Older_2, HIGH);
+            digitalWrite(LED_Older_3, HIGH);
+            digitalWrite(LED_Older_4, HIGH);
+            digitalWrite(LED_Older_5, HIGH);
             vTaskDelay(pdMS_TO_TICKS(500));
             digitalWrite(LED_Older_1, LOW);
             digitalWrite(LED_Older_2, LOW);
+            digitalWrite(LED_Older_3, LOW);
+            digitalWrite(LED_Older_4, LOW);
+            digitalWrite(LED_Older_5, LOW);
             vTaskDelay(pdMS_TO_TICKS(500));
         }
-
-        // 任务结束前确保 LED 关闭
-        digitalWrite(LED_Older_1, LOW);
-        digitalWrite(LED_Older_2, LOW);
-
-        // 切换到 LED_Kid_1 任务
-        xTaskCreate(blinkSingleLED, "Blink LED_Kid_1", 1024, NULL, 1, &blinkSingleLEDHandle);
-        vTaskDelete(NULL); // 删除当前任务
+        xTaskCreate(blinkOlder_2LED, "Blink LED_older_2", 128, NULL, 1, &blinkOlder_2LEDHandle);
+        vTaskDelete(NULL); 
     }
 }
 
-// 任务2: LED_Kid_1 闪烁 5 秒
-void blinkSingleLED(void *parameter) {
+void blinkOlder_2LED(void *parameter) {
     while (1) {
-        for (int i = 0; i < 10; i++) { // 5s 闪烁（500ms * 10 次）
-            digitalWrite(LED_Kid_1, HIGH);
+        for (int i = 0; i < 10; i++) { 
+            digitalWrite(LED_Older_6, HIGH);
+            digitalWrite(LED_Older_7, HIGH);
+            digitalWrite(LED_Older_8, HIGH);
+            digitalWrite(LED_Older_9, HIGH);
+            digitalWrite(LED_Older_10, HIGH);
             vTaskDelay(pdMS_TO_TICKS(500));
-            digitalWrite(LED_Kid_1, LOW);
+            digitalWrite(LED_Older_6, LOW);
+            digitalWrite(LED_Older_7, LOW);
+            digitalWrite(LED_Older_8, LOW);
+            digitalWrite(LED_Older_9, LOW);
+            digitalWrite(LED_Older_10, LOW);
             vTaskDelay(pdMS_TO_TICKS(500));
         }
 
-        // 任务结束前确保 LED 关闭
-        digitalWrite(LED_Kid_1, LOW);
+        xTaskCreate(blinkKid_1LED, "Blink LED_Kid_1", 128, NULL, 1, &blinkKid_1LEDHandle);
+        vTaskDelete(NULL); 
+    }
+}
 
-        // 切换回 LED_Older_1 和 LED_Older_2 任务
-        xTaskCreate(blinkTwoLEDs, "Blink Two LEDs", 1024, NULL, 1, &blinkTwoLEDsHandle);
-        vTaskDelete(NULL); // 删除当前任务
+void blinkKid_1LED(void *parameter) {
+    while (1) {
+        for (int i = 0; i < 10; i++) {
+            digitalWrite(LED_Kid_1, HIGH);
+            digitalWrite(LED_Kid_2, HIGH);
+            digitalWrite(LED_Kid_3, HIGH);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            digitalWrite(LED_Kid_1, LOW);
+            digitalWrite(LED_Kid_2, LOW);
+            digitalWrite(LED_Kid_3, LOW);
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
+        xTaskCreate(blinkKid_2LED, "Blink Two LEDs", 128, NULL, 1, &blinkKid_2LEDHandle);
+        vTaskDelete(NULL);
+    }
+}
+
+void blinkKid_2LED(void *parameter) {
+    while (1) {
+        for (int i = 0; i < 10; i++) {
+            digitalWrite(LED_Kid_4, HIGH);
+            digitalWrite(LED_Kid_5, HIGH);
+            digitalWrite(LED_Kid_6, HIGH);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            digitalWrite(LED_Kid_4, LOW);
+            digitalWrite(LED_Kid_5, LOW);
+            digitalWrite(LED_Kid_6, LOW);
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
+        xTaskCreate(blinkOlder_1LED, "Blink Two LEDs", 128, NULL, 1, &blinkOlder_1LEDHandle);
+        vTaskDelete(NULL);
     }
 }
 
 void setup() {
-    // 设置 LED 引脚为输出模式
-    pinMode(LED_Older_1, OUTPUT);
-    pinMode(LED_Older_2, OUTPUT);
-    pinMode(LED_Kid_1, OUTPUT);
+  pinMode(LED_Older_1, OUTPUT);
+  pinMode(LED_Older_2, OUTPUT);
+  pinMode(LED_Older_3, OUTPUT);
+  pinMode(LED_Older_4, OUTPUT);
+  pinMode(LED_Older_5, OUTPUT);
 
-    Serial.begin(115200);
+  pinMode(LED_Kid_1, OUTPUT);
+  pinMode(LED_Kid_2, OUTPUT);
+  pinMode(LED_Kid_3, OUTPUT);
 
-    // **开始第一个任务**
-    xTaskCreate(blinkTwoLEDs, "Blink Two LEDs", 1024, NULL, 1, &blinkTwoLEDsHandle);
+  pinMode(LED_Older_6, OUTPUT);
+  pinMode(LED_Older_7, OUTPUT);
+  pinMode(LED_Older_8, OUTPUT);
+  pinMode(LED_Older_9, OUTPUT);
+  pinMode(LED_Older_10, OUTPUT);
+
+  pinMode(LED_Kid_4, OUTPUT);
+  pinMode(LED_Kid_5, OUTPUT);
+  pinMode(LED_Kid_6, OUTPUT);
+
+  Serial.begin(115200);
+
+  xTaskCreate(blinkOlder_1LED, "Blink Two LEDs", 128, NULL, 1, &blinkOlder_1LEDHandle);
+  vTaskStartScheduler();
 }
 
 void loop() {
-    // 任务由 FreeRTOS 负责管理，loop() 不执行任何操作
     vTaskDelay(pdMS_TO_TICKS(1000));
 }
--- MCU_code/Test_code/LED_test/LED_test.ino
+++ MCU_code/Test_code/LED_test/LED_test.ino
@@ -1,25 +1,45 @@
-#define LED_Older_1 1
-#define LED_Older_2 25
-#define LED_Older_3 26
-#define LED_Older_4 27
-#define LED_Older_5 9
+#define LED_Older_1 6
+#define LED_Older_2 7
+#define LED_Older_3 8
+#define LED_Older_4 9
+#define LED_Older_5 10
 
-#define LED_Kid_1 10
-#define LED_Kid_2 13
-#define LED_Kid_3 5
+#define LED_Kid_1 11
+#define LED_Kid_2 12
+#define LED_Kid_3 13
 
-#define Button 3
+#define LED_Older_6 0
+#define LED_Older_7 1
+#define LED_Older_8 2
+#define LED_Older_9 3
+#define LED_Older_10 4
+
+#define LED_Kid_4 A3
+#define LED_Kid_5 A4
+#define LED_Kid_6 A5
+
+#define Button 5
 
 void setup() {
     pinMode(LED_Older_1, OUTPUT);
     pinMode(LED_Older_2, OUTPUT);
     pinMode(LED_Older_3, OUTPUT);
-    // pinMode(LED_Older_4, OUTPUT);
-    //pinMode(LED_Older_5, OUTPUT);
+    pinMode(LED_Older_4, OUTPUT);
+    pinMode(LED_Older_5, OUTPUT);
 
-    //pinMode(LED_Kid_1, OUTPUT);
-    // pinMode(LED_Kid_2, OUTPUT);
-    // pinMode(LED_Kid_3, OUTPUT);
+    pinMode(LED_Kid_1, OUTPUT);
+    pinMode(LED_Kid_2, OUTPUT);
+    pinMode(LED_Kid_3, OUTPUT);
+
+    pinMode(LED_Older_6, OUTPUT);
+    pinMode(LED_Older_7, OUTPUT);
+    pinMode(LED_Older_8, OUTPUT);
+    pinMode(LED_Older_9, OUTPUT);
+    pinMode(LED_Older_10, OUTPUT);
+
+    pinMode(LED_Kid_4, OUTPUT);
+    pinMode(LED_Kid_5, OUTPUT);
+    pinMode(LED_Kid_6, OUTPUT);
 
 }
 
@@ -27,24 +47,41 @@ void loop() {
     digitalWrite(LED_Older_1, HIGH);
     digitalWrite(LED_Older_2, HIGH);
     digitalWrite(LED_Older_3, HIGH);
-    // digitalWrite(LED_Older_4, HIGH);
-    //digitalWrite(LED_Older_5, HIGH);
+    digitalWrite(LED_Older_4, HIGH);
+    digitalWrite(LED_Older_5, HIGH);
+    digitalWrite(LED_Older_6, HIGH);
+    digitalWrite(LED_Older_7, HIGH);
+    digitalWrite(LED_Older_8, HIGH);
+    digitalWrite(LED_Older_9, HIGH);
+    digitalWrite(LED_Older_10, HIGH);
 
-    //digitalWrite(LED_Kid_1, LOW);
-    // digitalWrite(LED_Kid_2, LOW);
-    // digitalWrite(LED_Kid_3, LOW);
+    digitalWrite(LED_Kid_1, LOW);
+    digitalWrite(LED_Kid_2, LOW);
+    digitalWrite(LED_Kid_3, LOW);
+    digitalWrite(LED_Kid_4, LOW);
+    digitalWrite(LED_Kid_5, LOW);
+    digitalWrite(LED_Kid_6, LOW);
+
 
     delay(3000); 
 
     digitalWrite(LED_Older_1, LOW);
     digitalWrite(LED_Older_2, LOW);
     digitalWrite(LED_Older_3, LOW);
-    //digitalWrite(LED_Older_4, LOW);
-    //digitalWrite(LED_Older_5, LOW);
+    digitalWrite(LED_Older_4, LOW);
+    digitalWrite(LED_Older_5, LOW);
+    digitalWrite(LED_Older_6, LOW);
+    digitalWrite(LED_Older_7, LOW);
+    digitalWrite(LED_Older_8, LOW);
+    digitalWrite(LED_Older_9, LOW);
+    digitalWrite(LED_Older_10, LOW);
 
-    //digitalWrite(LED_Kid_1, HIGH);
-    // digitalWrite(LED_Kid_2, HIGH);
-    // digitalWrite(LED_Kid_3, HIGH);
+    digitalWrite(LED_Kid_1, HIGH);
+    digitalWrite(LED_Kid_2, HIGH);
+    digitalWrite(LED_Kid_3, HIGH);
+    digitalWrite(LED_Kid_4, HIGH);
+    digitalWrite(LED_Kid_5, HIGH);
+    digitalWrite(LED_Kid_6, HIGH);
 
     delay(3000);
 }
