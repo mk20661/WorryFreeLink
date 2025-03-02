a5e51003189f8c85cd4bbd398895df5b56f4f653
Hongbing  Qiu <qakightbing@163.com>
2025-03-02 12:22:23 +0000

    add aduio  function

---
+++ MCU_code/Test_code/add_aduio_function_version/add_aduio_function_version.ino
@@ -0,0 +1,255 @@
+#include <Arduino.h>
+#include <vector>
+#include <Arduino_FreeRTOS.h>
+#include <SoftwareSerial.h>
+
+
+//aduio chip
+SoftwareSerial playerSerial(2,3);
+
+
+#define Button A3
+#define RedButton A2
+
+#define OlderSelected 0
+#define KidSelected_1 8
+#define KidSelected_2 12
+
+#define LED_Older_1 1
+#define LED_Older_2 4
+#define LED_Older_3 5
+#define LED_Older_4 6
+#define LED_Older_5 7
+
+#define LED_Kid_1 9
+#define LED_Kid_2 10
+#define LED_Kid_3 11
+
+#define LED_Kid_4 13
+#define LED_Kid_5 ((int)A5)
+#define LED_Kid_6 ((int)A4)
+
+#define eating "/eating.mp3"
+#define exercising "/exercising.mp3"
+#define sleeping "/sleeping.mp3"
+#define social "/social.mp3"
+#define others "/others.mp3"
+
+std::vector<int> selectPins = {OlderSelected,KidSelected_1,KidSelected_2};
+std::vector<int> olderLEDPins = {LED_Older_1, LED_Older_2, LED_Older_3, LED_Older_4, LED_Older_5};
+std::vector<int> kidLEDPins1 = {LED_Kid_1,LED_Kid_2,LED_Kid_3};
+std::vector<int> kidLEDPins2 = {LED_Kid_4,LED_Kid_5,LED_Kid_6};
+std::vector<String> attentionAudio = {eating,sleeping,exercising,social,others};
+
+TaskHandle_t ledTaskHandle;
+TaskHandle_t buttonTaskHandle;
+TaskHandle_t selectStatusHandle;
+TaskHandle_t selectButtonHandle;
+
+volatile int currentMode = 0; 
+volatile bool taskStopped = false;
+volatile bool buttonPressed = false;
+volatile bool selectButtonPressed = false;
+volatile int currentOlederLEDIndex_case1 = 0;
+volatile int currentKidLEDIndex_case1 = 0;
+volatile int currentKidLEDIndex_case2 = 0; 
+
+void ledTask(void *parameter) {
+  while(1){
+    if (taskStopped) { 
+      vTaskDelete(NULL); 
+    }
+
+    currentMode = 1;
+    for (int i = 0; i < 5; i++) {
+        if (taskStopped) { vTaskDelete(NULL); }
+        for (int pin : {OlderSelected,LED_Older_1, LED_Older_2, LED_Older_3, LED_Older_4, LED_Older_5}) {
+          digitalWrite(pin, HIGH);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+        for (int pin : {OlderSelected,LED_Older_1, LED_Older_2, LED_Older_3, LED_Older_4, LED_Older_5}) {
+          digitalWrite(pin, LOW);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+
+    currentMode = 2;
+    for (int i = 0; i < 5; i++) {
+        if (taskStopped) { vTaskDelete(NULL); }
+        for (int pin : {KidSelected_1,LED_Kid_1, LED_Kid_2, LED_Kid_3}) {
+            digitalWrite(pin, HIGH);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+        for (int pin : {KidSelected_1,LED_Kid_1, LED_Kid_2, LED_Kid_3}) {
+            digitalWrite(pin, LOW);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+
+    currentMode = 3;
+    for (int i = 0; i < 5; i++) {
+        if (taskStopped) { vTaskDelete(NULL); }
+        for (int pin : {KidSelected_2,LED_Kid_4, LED_Kid_5, LED_Kid_6}) {
+          digitalWrite(pin, HIGH);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+        for (int pin : {KidSelected_2,LED_Kid_4, LED_Kid_5, LED_Kid_6}) {
+          digitalWrite(pin, LOW);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+  }  
+}
+
+void checkButton1(void *parameter) {
+    while (1) {
+        int buttonState = digitalRead(Button);
+
+        if (!buttonPressed && buttonState == HIGH) { 
+            buttonPressed = true;
+            taskStopped = true; 
+
+            for (int pin : {OlderSelected,KidSelected_1,KidSelected_2,
+                            LED_Older_1, LED_Older_2, LED_Older_3, LED_Older_4, LED_Older_5,
+                            LED_Kid_1, LED_Kid_2, LED_Kid_3, LED_Kid_4, LED_Kid_5, LED_Kid_6}) {
+              digitalWrite(pin, LOW);
+            }
+
+            if (ledTaskHandle != NULL) {
+                vTaskDelete(ledTaskHandle);
+                ledTaskHandle = NULL;
+            }
+
+            if (selectStatusHandle == NULL) {
+                xTaskCreate(selectStatus, "selectStatus", 256, NULL, 1, &selectStatusHandle);
+            }
+
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(100)); 
+    }
+}
+
+void selectStatus(void *parameter) {
+  vTaskDelay(pdMS_TO_TICKS(1000));
+  xTaskCreate(selectButton, "selectButtonCheck", 256, NULL, 1, &selectButtonHandle);
+  while (1) {
+    switch (currentMode) {
+        case 1:
+        digitalWrite(OlderSelected,HIGH);
+          for (int i = 0; i < 5; i++) {
+              if (selectButtonPressed) break;
+              digitalWrite(olderLEDPins[currentOlederLEDIndex_case1], HIGH);
+              vTaskDelay(pdMS_TO_TICKS(500));
+              digitalWrite(olderLEDPins[currentOlederLEDIndex_case1], LOW);
+              vTaskDelay(pdMS_TO_TICKS(500));
+              if(i == 4) {
+                playFile(attentionAudio[currentOlederLEDIndex_case1]);
+              }
+          }
+
+          if (!selectButtonPressed) {
+              digitalWrite(olderLEDPins[currentOlederLEDIndex_case1], HIGH);
+          }
+
+          while (!selectButtonPressed) {
+              vTaskDelay(pdMS_TO_TICKS(100));
+          }
+
+          digitalWrite(olderLEDPins[currentOlederLEDIndex_case1], LOW);
+          selectButtonPressed = false;
+          currentOlederLEDIndex_case1 = (currentOlederLEDIndex_case1 + 1) % 5;
+          break;
+
+        case 2:
+        digitalWrite(KidSelected_1,HIGH);
+         for (int i = 0; i < 5; i++) {
+            if (selectButtonPressed) break;
+              digitalWrite(kidLEDPins1[currentKidLEDIndex_case1], HIGH);
+              vTaskDelay(pdMS_TO_TICKS(500));
+              digitalWrite(kidLEDPins1[currentKidLEDIndex_case1], LOW);
+              vTaskDelay(pdMS_TO_TICKS(500));
+            }
+
+            if (!selectButtonPressed) {
+              digitalWrite(kidLEDPins1[currentKidLEDIndex_case1], HIGH);
+            }
+
+            while (!selectButtonPressed) {
+              vTaskDelay(pdMS_TO_TICKS(100));
+            }
+
+            digitalWrite(kidLEDPins1[currentKidLEDIndex_case1], LOW);
+            selectButtonPressed = false;
+            currentKidLEDIndex_case1 = (currentKidLEDIndex_case1 + 1) % 3;
+            break;
+
+        case 3:
+        digitalWrite(KidSelected_2,HIGH);
+         for (int i = 0; i < 5; i++) {
+            if (selectButtonPressed) break;
+              digitalWrite(kidLEDPins2[currentKidLEDIndex_case2], HIGH);
+              vTaskDelay(pdMS_TO_TICKS(500));
+              digitalWrite(kidLEDPins2[currentKidLEDIndex_case2], LOW);
+              vTaskDelay(pdMS_TO_TICKS(500));
+            }
+
+            if (!selectButtonPressed) {
+              digitalWrite(kidLEDPins2[currentKidLEDIndex_case2], HIGH);
+            }
+
+            while (!selectButtonPressed) {
+              vTaskDelay(pdMS_TO_TICKS(100));
+            }
+
+            digitalWrite(kidLEDPins2[currentKidLEDIndex_case2], LOW);
+            selectButtonPressed = false;
+            currentKidLEDIndex_case2 = 3 + ((currentKidLEDIndex_case2 - 3 + 1) % 3);
+            break;
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+void selectButton(void *parameter) {
+    while (1) {  
+        int buttonState = digitalRead(Button);
+        if (!selectButtonPressed && buttonState == HIGH) { 
+            selectButtonPressed = true;
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));  
+    }
+}
+
+void setup() {
+  Serial.begin(115200);
+playerSerial.begin(9600);
+  delay(1000);
+  sendCommand("AT+PLAYMODE=3");
+  sendCommand("AT+LED=ON");
+  sendCommand("AT+VOL=13");
+  for (int pin : {OlderSelected,KidSelected_1,KidSelected_2,
+                  LED_Older_1, LED_Older_2, LED_Older_3, LED_Older_4, LED_Older_5,
+                  LED_Kid_1, LED_Kid_2, LED_Kid_3, LED_Kid_4, LED_Kid_5, LED_Kid_6}) {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, LOW);
+  }
+  pinMode(Button, INPUT_PULLUP);  
+
+  xTaskCreate(ledTask, "LED Task", 256, NULL, 1, &ledTaskHandle);
+  xTaskCreate(checkButton1, "Check Button", 256, NULL, 1, &buttonTaskHandle);
+  vTaskStartScheduler();
+}
+void loop() {
+    vTaskDelay(pdMS_TO_TICKS(100)); 
+}
+
+void playFile(String fileName) {
+  sendCommand("AT+PLAYFILE=" + fileName);
+}
+
+void sendCommand(String command) {
+  playerSerial.print(command + "\r\n");
+  Serial.println("Sent: " + command);
+}
